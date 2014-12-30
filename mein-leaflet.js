2fab686b8d00b24e11fa0d5c0848e21a577d3f50
Markus Strauß <msstrauss@gmx.de>
2014-12-30 20:09:46 +0100

    COde cosmetic

--- mein-leaflet.js
+++ mein-leaflet.js
@@ -1,11 +1,12 @@
 "use strict";
 
 $(document).ready(function(){
+  // Create Map
   var map = L.map('map').setView([50.739, 7.117], 13);
   
+  // Connect osm with leaflet
   var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   var osmLayer = new L.TileLayer(osmUrl, {maxZoom:18, attribution:"OpenStreetmap"});
-  
   osmLayer.addTo(map);
   
 });
